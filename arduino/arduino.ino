5780f791c83bcaac46ead07ee87d9775c92e928c
Denis Balogh <denbalogh@gmail.com>
2023-10-15 17:36:13 +0200

    updates

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -8,39 +8,12 @@ void setup() {
   Serial.begin(115200);
   sensor.begin();
   sensor.setGain(3);
+  sensor.setIntegrationTime(50);
 }
 
 void loop() {
   if (Serial.available() && Serial.readString() == "get") {
     String buff = "";
-    sensor.setIntegrationTime(50);
-    sensor.takeMeasurementsWithBulb();
-    buff += String(sensor.getCalibratedR(), decimals) + ",";
-    buff += String(sensor.getCalibratedS(), decimals) + ",";
-    buff += String(sensor.getCalibratedT(), decimals) + ",";
-    buff += String(sensor.getCalibratedU(), decimals) + ",";
-    buff += String(sensor.getCalibratedV(), decimals) + ",";
-    buff += String(sensor.getCalibratedW(), decimals) + "|";
-
-    sensor.setIntegrationTime(100);
-    sensor.takeMeasurementsWithBulb();
-    buff += String(sensor.getCalibratedR(), decimals) + ",";
-    buff += String(sensor.getCalibratedS(), decimals) + ",";
-    buff += String(sensor.getCalibratedT(), decimals) + ",";
-    buff += String(sensor.getCalibratedU(), decimals) + ",";
-    buff += String(sensor.getCalibratedV(), decimals) + ",";
-    buff += String(sensor.getCalibratedW(), decimals) + "|";
-
-    sensor.setIntegrationTime(150);
-    sensor.takeMeasurementsWithBulb();
-    buff += String(sensor.getCalibratedR(), decimals) + ",";
-    buff += String(sensor.getCalibratedS(), decimals) + ",";
-    buff += String(sensor.getCalibratedT(), decimals) + ",";
-    buff += String(sensor.getCalibratedU(), decimals) + ",";
-    buff += String(sensor.getCalibratedV(), decimals) + ",";
-    buff += String(sensor.getCalibratedW(), decimals) + "|";
-
-    sensor.setIntegrationTime(200);
     sensor.takeMeasurementsWithBulb();
     buff += String(sensor.getCalibratedR(), decimals) + ",";
     buff += String(sensor.getCalibratedS(), decimals) + ",";
---
+++ data/batch1/README.md
@@ -0,0 +1,13 @@
+### First batch of data
+
+I tried getting 4 types of sensor data, each type having a different intergration time.
+
+I chose integration time values 50, 100, 150, 200.
+
+My goal was to test whether there is a difference in quality of the data based on the integration time.
+
+I collected data when I was fasted and after meal.
+
+There are statistically significant differences in the collected data when fasted and when satiated(after meal).
+
+The integration time doesn't seem to have an effect on the statistical difference however, so I decided to use the lowest value for the next data collection, because it takes less amount of time and therefore results in quicker data collection.
