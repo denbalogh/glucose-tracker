139fc84064d361cb5f92f649faf7577e85a07e5e
Denis Balogh <denbalogh@gmail.com>
2023-11-27 12:15:53 +0100

    update

--- triad_sensor/arduino/arduino.ino
+++ triad_sensor/arduino/arduino.ino
@@ -24,10 +24,7 @@ void loop() {
   if (Serial.available() && Serial.readString() == "get") {
     String buff = "";
     
-    // Take UV measurements
-    sensor.enableBulb(AS7265x_LED_UV);
-    sensor.takeMeasurements();
-    sensor.disableBulb(AS7265x_LED_UV);
+    sensor.takeMeasurementsWithBulb();
 
     buff += String(sensor.getCalibratedA(), decimals) + ","; //410nm
     buff += String(sensor.getCalibratedB(), decimals) + ","; //435nm
@@ -35,30 +32,18 @@ void loop() {
     buff += String(sensor.getCalibratedD(), decimals) + ","; //485nm
     buff += String(sensor.getCalibratedE(), decimals) + ","; //510nm
     buff += String(sensor.getCalibratedF(), decimals) + ","; //535nm
-
-    // Take VIS measurements
-    sensor.enableBulb(AS7265x_LED_WHITE);
-    sensor.takeMeasurements();
-    sensor.disableBulb(AS7265x_LED_WHITE);
-
     buff += String(sensor.getCalibratedG(), decimals) + ","; //560nm
     buff += String(sensor.getCalibratedH(), decimals) + ","; //585nm
-    buff += String(sensor.getCalibratedR(), decimals) + ","; //610nm
-    buff += String(sensor.getCalibratedI(), decimals) + ","; //645nm
-    buff += String(sensor.getCalibratedS(), decimals) + ","; //680nm
-    buff += String(sensor.getCalibratedJ(), decimals) + ","; //705nm
-
-    // Take IR measurements
-    sensor.enableBulb(AS7265x_LED_IR);
-    sensor.takeMeasurements();
-    sensor.disableBulb(AS7265x_LED_IR);
-
-    buff += String(sensor.getCalibratedT(), decimals) + ","; //730nm
-    buff += String(sensor.getCalibratedU(), decimals) + ","; //760nm
-    buff += String(sensor.getCalibratedV(), decimals) + ","; //810nm
-    buff += String(sensor.getCalibratedW(), decimals) + ","; //860nm
-    buff += String(sensor.getCalibratedK(), decimals) + ","; //900nm
-    buff += String(sensor.getCalibratedL(), decimals) + ","; //940nm
+    buff += String(sensor.getCalibratedI(), decimals) + ","; //610nm
+    buff += String(sensor.getCalibratedJ(), decimals) + ","; //645nm
+    buff += String(sensor.getCalibratedK(), decimals) + ","; //680nm
+    buff += String(sensor.getCalibratedL(), decimals) + ","; //705nm
+    buff += String(sensor.getCalibratedR(), decimals) + ","; //730nm
+    buff += String(sensor.getCalibratedS(), decimals) + ","; //760nm
+    buff += String(sensor.getCalibratedT(), decimals) + ","; //810nm
+    buff += String(sensor.getCalibratedU(), decimals) + ","; //860nm
+    buff += String(sensor.getCalibratedV(), decimals) + ","; //900nm
+    buff += String(sensor.getCalibratedW(), decimals) + ","; //940nm
 
     // Get temperature
     buff += String(sensor.getTemperatureAverage(), decimals);
---
+++ triad_sensor/glucose_measuring/README.md
@@ -0,0 +1,1 @@
+Blood glucose is maintained at a very constant level between 4 and 6.5 mmol/l. Occasionally, it may dip slightly lower than this to 3.5 mmol/l in young children who fast overnight.
